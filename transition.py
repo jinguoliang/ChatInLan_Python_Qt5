23646f8089a1cf029c1b88f33635816e175cd165
Jinux <jinguol999@163.com>
2018-11-11 22:16:35 +0800

    extract the server port and block size

--- transition.py
+++ transition.py
@@ -1,20 +1,24 @@
 import socket
 
+BLOCK_SIZE = 1024
+
+TRANSITION_SERVER_PORT = 8832
+
 
 class Server:
     def __init__(self):
         self.server_sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
     def listen(self):
-        self.server_sock.bind(("", 8832))
+        self.server_sock.bind(("", TRANSITION_SERVER_PORT))
         self.server_sock.listen()
 
         while True:
             client_sock, address = self.server_sock.accept()
-            data = client_sock.recv(32).decode()
+            data = client_sock.recv(BLOCK_SIZE).decode()
             while data:
                 print(data)
-                data = client_sock.recv(1024).decode()
+                data = client_sock.recv(BLOCK_SIZE).decode()
 
 
 class Client:
@@ -23,7 +27,7 @@ class Client:
 
     def send(self, address, path):
         data = self.getContent(path)
-        self.client_sock.connect((address[0], 8832))
+        self.client_sock.connect((address[0], TRANSITION_SERVER_PORT))
         self.client_sock.send(data)
 
     def getContent(self, path):
