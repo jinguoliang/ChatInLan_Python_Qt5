8a4a2f4593f2c0170a958881efc1232d30f268af
Jinux <jinguol999@163.com>
2018-11-11 22:07:57 +0800

    send and receive file data

--- transition.py
+++ transition.py
@@ -6,23 +6,26 @@ class Server:
         self.server_sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
     def listen(self):
-        print("listen")
         self.server_sock.bind(("", 8832))
-        print("binded")
         self.server_sock.listen()
-        print("listened")
 
         while True:
             client_sock, address = self.server_sock.accept()
-            print(address)
-            data = client_sock.recv(1024).decode()
-            print(data)
+            data = client_sock.recv(32).decode()
+            while data:
+                print(data)
+                data = client_sock.recv(1024).decode()
+
 
 class Client:
     def __init__(self):
         self.client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def send(self, address):
-        print(address)
+    def send(self, address, path):
+        data = self.getContent(path)
         self.client_sock.connect((address[0], 8832))
-        self.client_sock.send("haha".encode())
+        self.client_sock.send(data)
+
+    def getContent(self, path):
+        f = open(path, 'rb')
+        return f.read()
